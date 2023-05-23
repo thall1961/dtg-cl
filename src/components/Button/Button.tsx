27f0ebd2defb114e93c1efb9d5a33eac0878c940
Tom Hall <thall1961@gmail.com>
2023-05-23 16:13:20 -0500

    initial commit

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {ButtonProps} from "./Button.types";
+
+export const Button: React.FC<ButtonProps> = (
+    {
+        size,
+        primary,
+        disabled,
+        text,
+        onClick,
+        ...props
+    }
+) => {
+    let element = <><></>
+    </>;
+    return element;
+};
